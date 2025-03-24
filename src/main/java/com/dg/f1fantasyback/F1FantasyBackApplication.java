277f096c8e01a53b124d136b9736db05c8aaf968
Dimitri <dimitri.guitton.dev@gmail.com>
2025-03-24 11:33:17 +0100

    initialize f1-fantasy-back project with Spring Boot and PostgreSQL setup

---
+++ src/main/java/com/dg/f1fantasyback/F1FantasyBackApplication.java
@@ -0,0 +1,13 @@
+package com.dg.f1fantasyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1FantasyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1FantasyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dg/f1fantasyback/F1FantasyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.f1fantasyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1FantasyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
