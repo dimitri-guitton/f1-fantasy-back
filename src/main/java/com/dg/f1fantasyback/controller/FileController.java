922c27fae0d8cce0ea1abe78760f019946ce496f
Dimitri <dimitri.guitton.dev@gmail.com>
2025-03-26 18:12:01 +0100

    feat(FileStorage): implement file upload and retrieval functionality

---
+++ src/main/java/com/dg/f1fantasyback/controller/FileController.java
@@ -0,0 +1,39 @@
+package com.dg.f1fantasyback.controller;
+
+import com.dg.f1fantasyback.service.FileStorageService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class FileController {
+    private final FileStorageService fileStorageService;
+
+    public FileController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
+    }
+
+    @GetMapping("/images/{imageName}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String imageName) {
+        try {
+            byte[] image = fileStorageService.loadFileAsBytes(imageName);
+            String contentType = "image/jpeg";
+            if (imageName.endsWith(".png")) {
+                contentType = "image/png";
+            } else if (imageName.endsWith(".gif")) {
+                contentType = "image/gif";
+            }
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_TYPE, contentType)
+                    .body(image);
+        } catch (IOException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/dg/f1fantasyback/service/FileStorageService.java
@@ -0,0 +1,38 @@
+package com.dg.f1fantasyback.service;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    public String storeFile(MultipartFile file) throws IOException {
+        // Générer un nom unique pour l'image
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        Path filePath = Paths.get(uploadDir).resolve(fileName);
+
+        // Créer le dossier s'il n'existe pas
+        Files.createDirectories(filePath.getParent());
+
+        // Sauvegarder le fichier
+        Files.copy(file.getInputStream(), filePath);
+
+        return fileName; // Retourne le nom du fichier sauvegardé
+    }
+
+    public byte[] loadFileAsBytes(String fileName) throws IOException {
+        Path filePath = Paths.get(uploadDir).resolve(fileName);
+        return Files.readAllBytes(filePath);
+    }
+}
